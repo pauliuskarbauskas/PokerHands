38987ef2af0158a22459393b317c719bf88fb692
paulius karbauskas <pauliuskarbauskas@egmail.com>
2020-09-19 13:01:44 +0300

    Added functionality to parse strings from file into Cards

--- src/main/java/com/danske/Helper.java
+++ src/main/java/com/danske/Helper.java
@@ -34,4 +34,101 @@ public class Helper {
 		
 		return data;
 	}
+	
+	/**
+	 * Parses String line to two hands
+	 * @param line
+	 * @return a List of two hands
+	 */
+	public static List<Card[]> parseHands(String line) {
+		
+		List<Card[]> hands = new ArrayList<>();
+		Card[] hand1 = new Card[5];
+		Card[] hand2 = new Card[5];
+		
+		String[] parts = line.split(" ");
+		
+		for(int i = 0; i < 5; i++) {
+			hand1[i] = createCard(parts[i]);
+		}
+		
+		for(int i = 0; i < 5; i++) {
+			hand2[i] = createCard(parts[i+5]);
+		}
+		
+		hands.add(hand1);
+		hands.add(hand2);
+		
+		return hands;
+	}
+	
+	/**
+	 * Creates a Card Object from given string
+	 * @param cardString
+	 * @return Card Object
+	 */
+	private static Card createCard(String cardString) {
+		
+		Card card = new Card();
+		card.setValue(getCardValue(cardString.substring(0, 1)));
+		card.setSuit(getCardSuit(cardString.substring(1, 2)));
+
+		return card;
+	}
+	
+	/**
+	 * Evaluates card Value
+	 * @param value
+	 * @return Integer Based on the value
+	 */
+	private static int getCardValue(String value) {
+		
+		try {
+			return Integer.parseInt(value);
+		} catch(Exception e) {}
+		
+		if(value.equals("T")) {
+			return 10;
+		}
+		
+		if(value.equals("J")) {
+			return 11;
+		}
+		
+		if(value.equals("Q")) {
+			return 12;
+		}
+		
+		if(value.equals("K")) {
+			return 13;
+		}
+		
+		if(value.equals("A")) {
+			return 14;
+		}
+		
+		return 0;
+	}
+	
+	/**
+	 * Evaluates Card Suit
+	 * @param value
+	 * @return Suit enum based on the value
+	 */
+	private static Suit getCardSuit(String value) {
+		
+		if(value.equals("C")) {
+			return Suit.CLUB;
+		}
+		
+		if(value.equals("D")) {
+			return Suit.DIAMOND;
+		}
+		
+		if(value.equals("H")) {
+			return Suit.HEART;
+		}
+		
+		return Suit.SPADE;
+	}
 }
--- src/test/java/com/danske/HelperTest.java
+++ src/test/java/com/danske/HelperTest.java
@@ -1,7 +1,9 @@
 package com.danske;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
@@ -18,8 +20,56 @@ public class HelperTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testReadHandsMethodReadsDataFromFile() {
+		
+		List<String> data = Helper.readHands();
+
+		assertNotNull(data);	
+		assertEquals(1000, data.size());
+		assertEquals("8C TS KC 9H 4S 7D 2S 5D 3S AC", data.get(0));
+		assertEquals("AS KD 3D JD 8H 7C 8C 5C QD 6C", data.get(data.size()-1));
+		
+	}
+	
+	@Test
+	public void testParseHandsParsesCardValuesAndSuits() {
+		
+		List<Card[]> parsedHands = Helper.parseHands("1C 2D 3H 4S 5S 7D 2S 5D 3S AC");
+		
+		assertEquals(1, parsedHands.get(0)[0].getValue());
+		assertEquals(Suit.CLUB, parsedHands.get(0)[0].getSuit());
+		
+		assertEquals(2, parsedHands.get(0)[1].getValue());
+		assertEquals(Suit.DIAMOND, parsedHands.get(0)[1].getSuit());
+		
+		assertEquals(3, parsedHands.get(0)[2].getValue());
+		assertEquals(Suit.HEART, parsedHands.get(0)[2].getSuit());
+		
+		assertEquals(4, parsedHands.get(0)[3].getValue());
+		assertEquals(Suit.SPADE, parsedHands.get(0)[3].getSuit());
+		
+		assertEquals(5, parsedHands.get(0)[4].getValue());
+	}
+	
+	@Test
+	public void testParseHandsParsesCardValuesFromJackToAce() {
+		
+		List<Card[]> parsedHands = Helper.parseHands("1C 2D 3H 4S 5S JC QD KH AS AC");
+		
+		assertEquals(11, parsedHands.get(1)[0].getValue());
+		assertEquals(12, parsedHands.get(1)[1].getValue());
+		assertEquals(13, parsedHands.get(1)[2].getValue());
+		assertEquals(14, parsedHands.get(1)[3].getValue());
+		
+	}
+	
+	@Test
+	public void testParseHandsParsesInvalidCardValues() {
+		
+		List<Card[]> parsedHands = Helper.parseHands("## 2D 3H 4S 5S JC QD KH AS AC");
+		
+		assertEquals(0, parsedHands.get(0)[0].getValue());
+		
 	}
 
 }
